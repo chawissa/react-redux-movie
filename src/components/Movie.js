6d3d9f65b1e3e546f03f826d4232b04e0d232488
Charissa Ramirez <chawissa@gmail.com>
2021-07-25 11:52:16 -0400

    Debug image path

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,13 +2,15 @@ import React from "react";
 // STYLING AND ANIMATION
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { img_500 } from "../config/config";
 
 const Movie = ({ name, released, poster }) => {
   return (
     <div>
       <h3>{name}</h3>
       <p>{released}</p>
-      <img src={poster} alt={name} />
+      {/* <img alt={name} src={`${img_500}/${poster}`/> */}
+      <img alt={name} src={`${img_500}/${poster}`} />
     </div>
   );
 };
