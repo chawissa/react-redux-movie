c8b25a68df8913f2ae7b691c2a36fe32ea5a4de6
Charissa Ramirez <chawissa@gmail.com>
2021-07-27 13:05:20 -0400

    Images and description added

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { img_300, img_500, unavailable, noPicture } from "../config/config";
 // STYLING AND ANIMATION
 import styled from "styled-components";
 import { motion } from "framer-motion";
@@ -12,19 +13,51 @@ const MovieDetail = () => {
   return (
     <div className="card-shadow">
       <div className="detail">
-        <div className="rating">
-          <h3>
-            {movie.title}({(movie.release_date || "-----").substring(0, 4)})
-          </h3>
-          {movie.tagline && <i className="tagline">{movie.tagline}</i>}
-          <p>Rating: {movie.vote_average}</p>
-        </div>
+        <div className="stats">
+          <div className="rating">
+            <h3>
+              {movie.title} ({(movie.release_date || "-----").substring(0, 4)})
+            </h3>
 
-        <div className="info">
-          <div className="genres">
-            {movie.genres &&
-              movie.genres.map((genre) => <h3 key={genre.id}>{genre.name}</h3>)}
+            <p>Rating: {movie.vote_average}</p>
           </div>
+
+          <div className="info">
+            <div className="genres">
+              {movie.genres &&
+                movie.genres.map((genre) => (
+                  <h3 key={genre.id}>{genre.name}</h3>
+                ))}
+              {movie.runtime} minutes
+            </div>
+          </div>
+        </div>
+        <div className="media">
+          <img
+            src={
+              movie.backdrop_path
+                ? `${img_500}/${movie.poster_path}`
+                : unavailable
+            }
+            alt={movie.name}
+          />
+        </div>
+        {movie.tagline && <i className="tagline">{movie.tagline}</i>}
+        <div className="description">
+          <p>{movie.overview}</p>
+        </div>
+        <div className="carousel">
+          {credits.cast &&
+            credits.cast.map((cast) => (
+              <img
+                src={
+                  cast.profile_path
+                    ? `${img_300}/${cast.profile_path}`
+                    : noPicture
+                }
+                alt={cast?.name}
+              />
+            ))}
         </div>
       </div>
     </div>
